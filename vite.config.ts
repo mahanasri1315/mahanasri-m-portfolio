10507546b11c3a06547bc06f415ae285a05b35b9
Mahanasri M <mahanasri.m.cse.2024@snsct.org>
2026-08-25 22:31:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   base: "/mahanasri-m-portfolio/",
-
   tanstackStart: {
     server: { entry: "server" },
-
-    prerender: {
-      enabled: true,
-      crawlLinks: true,
-    },
   },
 });
